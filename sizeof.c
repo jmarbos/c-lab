0c2510a2a59811264e21c0dd943a50aebe8373ff
Jose M. Arbos <jmarbos@gmail.com>
2019-09-05 23:43:19 +0200

    Format output of sizeof.c in columns

--- sizeof.c
+++ sizeof.c
@@ -4,14 +4,14 @@
 
 int main()
 {
-	printf("char: %ld\n", sizeof(char)*8);
-	printf("short int: %ld\n", sizeof(short int)*8);
-	printf("int: %ld\n", sizeof(int)*8);
-	printf("long int: %ld\n", sizeof(long int)*8);
-	printf("float: %ld\n", sizeof(float)*8);
-	printf("double: %ld\n", sizeof(double)*8);
-	printf("long double: %ld\n", sizeof(long double)*8);
-	printf("void*: %ld\n", sizeof(void*)*8);
+	printf("char         %ld\n", sizeof(char)*8);
+	printf("short int    %ld\n", sizeof(short int)*8);
+	printf("int          %ld\n", sizeof(int)*8);
+	printf("long int     %ld\n", sizeof(long int)*8);
+	printf("float        %ld\n", sizeof(float)*8);
+	printf("double       %ld\n", sizeof(double)*8);
+	printf("long double  %ld\n", sizeof(long double)*8);
+	printf("void*        %ld\n", sizeof(void*)*8);
 
 	return 0;
 }
